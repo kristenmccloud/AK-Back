f8c5ae7ec37c53e67e777af31aec76fefcff2fbe
learn <learn@learn04-2.local>
2018-11-30 15:59:14 -0800

    edited controller and routes

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -38,6 +38,12 @@ class ApartmentsController < ApplicationController
     @apartment.destroy
   end
 
+  #GET /user/apartments/:user_id
+  def show_user_apartments
+    @user = User.find(params[:user_id])
+    render json: @user.apartments
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_apartment
@@ -46,6 +52,6 @@ class ApartmentsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def apartment_params
-      params.require(:apartment).permit(:street_a,:street_b,:city,:postal_code,:state,:country,:manager_name,:phone,:hours,:description,:long_desc)
+      params.require(:apartment).permit(:street_a,:street_b,:city,:postal_code,:state,:country,:manager_name,:phone,:hours,:description,:long_desc,:user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :apartments
+
+  get 'users/apartments/:user_id' => 'apartments#show_user_apartments'
 end
